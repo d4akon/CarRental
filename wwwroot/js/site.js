f1abc8b07a34c3b915def3901d4d16a5a098a5c1
Jakub Machnica <machnicajakub@gmail.com>
2023-02-17 00:11:09 +0100

    added javascript for range calendar, few changes to create html page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.onload = function () {
+    const pickupDateInput = document.querySelector("#pickupDate");
+    const returnDateInput = document.querySelector("#returnDate");
 
-// Write your JavaScript code.
+    const minDate = new Date();
+    const maxDate = new Date();
+    maxDate.setMonth(minDate.getMonth() + 3);
+
+    const minDateString = minDate.toISOString().slice(0, 10);
+    const maxDateString = maxDate.toISOString().slice(0, 10);
+
+    // Return date
+    const minReturnDate = new Date();
+    const maxReturnDate = new Date();
+
+    minReturnDate.setDate(maxDate.getDate() + 1);
+
+    maxReturnDate.setDate(minReturnDate.getDate());
+    maxReturnDate.setMonth(minReturnDate.getMonth() + 3);
+
+    const minReturnDateString = minReturnDate.toISOString().slice(0, 10);
+    const maxReturnDateString = maxReturnDate.toISOString().slice(0, 10);
+
+    pickupDateInput.setAttribute("min", minDateString);
+    pickupDateInput.setAttribute("max", maxDateString);
+
+    returnDateInput.setAttribute("min", minReturnDateString);
+    returnDateInput.setAttribute("max", maxReturnDateString);
+}
