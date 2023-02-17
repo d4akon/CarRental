4df52f053a733da3dabbdb10c8ec978aea02298a
Szymon Maj <szymon.maj@icloud.com>
2023-02-17 18:05:49 +0100

    feat: photos in car index and car model in home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CarRental.Models;
+﻿using CarRental.Interfaces;
+using CarRental.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,26 @@ namespace CarRental.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICarService _carService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICarService carService)
         {
             _logger = logger;
+            _carService = carService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var cars = await _carService.GetAllCarsAsync();
+            var carsCount = cars.Count();
+            var carsToDisplay = new List<Car>();
+            var listNumbers = RandomNumGenerator(carsCount);
+            foreach (var num in listNumbers)
+            {
+                carsToDisplay.Add(cars[num]);
+            }
+
+            return View(carsToDisplay);
         }
 
         public IActionResult Privacy()
@@ -27,6 +39,22 @@ namespace CarRental.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static List<int> RandomNumGenerator(int carsCount)
+        {
+            var rand = new Random();
+            List<int> listNumbers = new List<int>();
+            int number;
+            for (int i = 0; i < 3; i++)
+            {
+                do
+                {
+                    number = rand.Next(1, carsCount);
+                } while (listNumbers.Contains(number));
+                listNumbers.Add(number);
+            }
+            return listNumbers;
         }
     }
 }
